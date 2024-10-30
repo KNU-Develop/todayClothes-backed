64289f1b3685d0b52ccd175d6fdcda9798993bba
rogi-rogi <yrkim6883@gmail.com>
2024-10-30 09:09:52 +0900

    feat - SIE 크롤링 기능 개발 완료 - SIE 제품 설명 크롤링 기능 개발 완료 - 크롤링 매니지먼트 클래스 추가 - 크롤링 스케줄링 시점 변경(매주 수요일 12 am) - clothe entity, dto 일부 변경

--- src/main/java/org/project/todayclothes/dto/crawling/ClotheDto.java
+++ src/main/java/org/project/todayclothes/dto/crawling/ClotheDto.java
@@ -10,27 +10,32 @@ public class ClotheDto {
     private String name;
     private Integer price;
     private Category category;
-    private String content;
+    private String description;
     private String link;
     private String imgUrl;
 
     @Builder
-    public ClotheDto(String name, Integer price, Category category, String content, String link, String imgUrl) {
+    public ClotheDto(String name, Integer price, Category category, String description, String link, String imgUrl) {
         this.name = name;
         this.price = price;
         this.category = category;
-        this.content = content;
+        this.description = description;
         this.link = link;
         this.imgUrl = imgUrl;
     }
 
+    public void updateDescription(String newDescription) {
+        if (newDescription == null || newDescription.isEmpty())
+            return;
+        this.description = newDescription;
+    }
     @Override
     public String toString() { // for test
         return "ClotheDto{" +
                 "name='" + name + '\'' +
                 ", price=" + price +
                 ", category=" + category +
-                ", content='" + content + '\'' +
+                ", description='" + description + '\'' +
                 ", link='" + link + '\'' +
                 ", imgUrl='" + imgUrl + '\'' +
                 '}';
---
+++ src/main/java/org/project/todayclothes/service/crawler/CrawlerManagementService.java
@@ -0,0 +1,37 @@
+package org.project.todayclothes.service.crawler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+
+@Service
+@RequiredArgsConstructor
+public class CrawlerManagementService {
+    @Value("${crawler.mode}")
+    private String MODE;
+
+//    private final ZaraCrawlerService zaraCrawlerService;
+    private final SieCrawlerService sieCrawlerService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void executeOnceOnStartup() {
+        if (MODE.equals("dev")) {
+            sieCrawlerService.crawling();
+//            zaraCrawlerService.crawlingProductHeader(driver);
+        }
+    }
+
+
+    @Scheduled(cron = "0 0 0 * * 3")
+    public void executePeriodically() {
+        if (MODE.equals("deploy")) {
+            sieCrawlerService.crawling();
+//            zaraCrawlerService.crawlingProductHeader(driver);
+        }
+    }
+}
