04df8ba30a7abc575188428b1897e0aff0a7b65e
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2024-11-13 03:12:20 +0900

    feat : 이미지 추천 연결 로그 추가

--- src/main/java/org/project/todayclothes/exception/code/ClotheErrorCode.java
+++ src/main/java/org/project/todayclothes/exception/code/ClotheErrorCode.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ClotheErrorCode implements ErrorCode{
     CLOTHES_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 옷을 찾을 수 없습니다."),
+    CLOTHES_NOT_RECOMMENDED(HttpStatus.NOT_FOUND, "추천받은 옷을 찾을 수 없습니다."),
     S3_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3에 파일을 업로드하는 중 오류가 발생했습니다."),
     S3_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3에 파일을 삭제하는 중 오류가 발생했습니다."),
     FILE_CONVERT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 변환 오류가 발생했습니다."),
