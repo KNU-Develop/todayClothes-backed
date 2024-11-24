89475c6ece5324a6f5981acb609ff3460f2c7711
rogi-rogi <yrkim6883@gmail.com>
2024-11-25 04:48:07 +0900

    update - 개발 환경에서의 크롬 드라이버 의존성 제거(자동 구동)

--- src/main/java/org/project/todayclothes/component/WebDriverFactory.java
+++ src/main/java/org/project/todayclothes/component/WebDriverFactory.java
@@ -24,7 +24,7 @@ public class WebDriverFactory {
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36");
 
         if ("dev".equals(MODE)) {
-            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+//            System.setProperty("webdriver.chrome.driver", chromeDriverPath); // 크롬드라이버 의존성 제거
             return new ChromeDriver(options);
         } else {
             return new RemoteWebDriver(new URL(chromeDriverPath), options);
